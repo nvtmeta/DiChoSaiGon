000c2e1b1faf57c62d17c0a004b998aa40ff2e0b
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-04-04 23:20:09 +0700

    video 6 done ( productid not render because of lacking data in sql server0

--- DiChoSaiGon/Controllers/HomeController.cs
+++ DiChoSaiGon/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace DiChoSaiGon.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
---
+++ DiChoSaiGon/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using DiChoSaiGon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DiChoSaiGon.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly dbMarketsContext _context;
+        public ProductController(dbMarketsContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+            {
+                return RedirectToAction("Index");
+            } else
+            {
+            return View(product);
+            }
+        }
+    }
+}
