62792c85c668c460e5240a367af9af378fed9faf
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-04-11 13:50:07 +0700

    carddonebuterroratsuccess

---
+++ DiChoSaiGon/Controllers/CheckoutController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiChoSaiGon.Extension;
+using DiChoSaiGon.Helpper;
+using DiChoSaiGon.Models;
+using DiChoSaiGon.ModelViews;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DiChoSaiGon.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly dbMarketsContext _context;
+        public CheckoutController(dbMarketsContext context)
+        {
+            _context = context;
+        }
+        public List<CartItem> GioHang
+        {
+            get
+            {
+                var gh = HttpContext.Session.Get<List<CartItem>>("GioHang");
+                if (gh == default(List<CartItem>))
+                {
+                    gh = new List<CartItem>();
+                }
+                return gh;
+            }
+        }
+
+
+
+
+
+        [Route("checkout.html", Name = "Checkout")]
+        public IActionResult Index(string returnUrl = null)
+        {
+            //Lay gio hang ra de xu ly
+            var cart = HttpContext.Session.Get<List<CartItem>>("GioHang");
+            var taikhoanID = HttpContext.Session.GetString("CustomerId");
+            MuaHangVM model = new MuaHangVM();
+            if (taikhoanID != null)
+            {
+                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
+                model.CustomerId = khachhang.CustomerId;
+                model.FullName = khachhang.FullName;
+                model.Email = khachhang.Email;
+                model.Phone = khachhang.Phone;
+            }
+            ViewBag.GioHang = cart;
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("checkout.html", Name = "Checkout")]
+        public IActionResult Index(int? id)
+        {
+            //Lay ra gio hang de xu ly
+            var cart = HttpContext.Session.Get<List<CartItem>>("GioHang"); 
+            var taikhoanID = HttpContext.Session.GetString("CustomerId");
+            MuaHangVM model = new MuaHangVM();
+            if (taikhoanID != null)
+            {
+                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
+                model.CustomerId = khachhang.CustomerId;
+                model.FullName = khachhang.FullName;
+                model.Email = khachhang.Email;
+                model.Phone = khachhang.Phone;
+
+                _context.Update(khachhang);
+                _context.SaveChanges();
+            }
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Khoi tao don hang
+                    Order donhang = new Order();
+                    donhang.CustomerId = model.CustomerId;
+
+                    donhang.OrderDate = DateTime.Now;
+                    donhang.TransactStatusId = 1;//Don hang moi
+                    donhang.Deleted = false;
+                    donhang.Paid = false;
+                    donhang.Note = Utilities.StripHTML(model.Note);
+                    donhang.TotalMoney = Convert.ToInt32(cart.Sum(x => x.TotalMoney));
+                    _context.Add(donhang);
+                    //tao danh sach don hang
+
+                    foreach (var item in cart)
+                    {
+                        OrderDetail orderDetail = new OrderDetail();
+                        orderDetail.OrderId = donhang.OrderId;
+                        orderDetail.ProductId = item.product.ProductId;
+                        orderDetail.Amount = item.amount;
+                        orderDetail.TotalMoney = donhang.TotalMoney;
+                        orderDetail.Price = item.product.Price;
+                        orderDetail.CreateDate = DateTime.Now;
+                        _context.Add(orderDetail);
+                    }
+                    //clear gio hang
+                    HttpContext.Session.Remove("GioHang");
+                    //Xuat thong bao
+                   if(true)
+                    {
+                        _context.SaveChanges();
+                        //cap nhat thong tin khach hang
+                        return RedirectToAction("Success");
+
+                    }
+                }
+            }
+            catch
+            {
+                return RedirectToAction("Success");
+            }
+            return RedirectToAction("Success");
+        }
+        [Route("dat-hang-thanh-cong.html", Name = "Success")]
+        public IActionResult Success()
+        {
+            try
+            {
+                var taikhoanID = HttpContext.Session.GetString("CustomerId");
+                if (string.IsNullOrEmpty(taikhoanID))
+                {
+                    return RedirectToAction("Login", "Accounts", new { returnUrl = "/dat-hang-thanh-cong.html" });
+                }
+                var khachhang = _context.Customers.AsNoTracking().SingleOrDefault(x => x.CustomerId == Convert.ToInt32(taikhoanID));
+                var donhang = _context.Orders
+                    .Where(x => x.CustomerId == Convert.ToInt32(taikhoanID))
+                    .OrderByDescending(x => x.OrderDate)
+                    .FirstOrDefault();
+                MuaHangSuccessVM successVM = new MuaHangSuccessVM();
+                successVM.FullName = khachhang.FullName;
+                successVM.DonHangID = donhang.OrderId;
+                successVM.Phone = khachhang.Phone;
+                return View(successVM);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+       
+    }
+}
---
+++ DiChoSaiGon/ModelView/CartItem.cs
@@ -0,0 +1,12 @@
+﻿using DiChoSaiGon.Models;
+using System;
+
+namespace DiChoSaiGon.ModelViews
+{
+    public class CartItem
+    {
+        public Product product { get; set; }
+        public int amount { get; set; }
+        public double TotalMoney => amount * product.Price.Value;
+    }
+}
